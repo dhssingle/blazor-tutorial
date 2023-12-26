192719d5978875fbf8a9f09711d64e07f6aa4e4f
dhssingle <dhssingle@gmail.com>
2023-12-26 21:23:55 +0800

    Create asp.net core web api

--- src/EmployeeManagement.Models/Employee.cs
+++ src/EmployeeManagement.Models/Employee.cs
@@ -7,6 +7,6 @@ public class Employee
     public string Email { get; set; }
     public DateTime DateOfBrith { get; set; }
     public Gender Gender { get; set; }
-    public Department Department { get; set; }
+    public int DepartmentId { get; set; }
     public string PhotoPath { get; set; }
 }
